4432f0cbad63a43dc4503bffd52409e8ef9f3a39
BrigtHaavardstun <Brigt_Arve@outlook.com>
2024-01-21 15:47:41 +0100

    [server/endpoint] Add counterfactual endpoint

--- pythonServer/server.py
+++ pythonServer/server.py
@@ -7,23 +7,41 @@ app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
 
+def _find_mean_array(ds):
+    mean_array = [0]*len(ds[0])
+    for i in range(0, len(ds[0])):
+        curr_sum = 0
+        for j in range(0, len(ds)):
+            curr_sum += ds[j][i]
+        mean_array[i] = curr_sum/len(ds)
+
+
+TARGET_DIFF_VALUE = 50
+
+
+def _evaluate(ds):
+    diff = [0]*len(ds[0])
+    for i in range(0, len(ds[0])):
+        min_curr = min([ds[j][i] for j in range(0, len(ds))])
+        max_curr = max([ds[j][i] for j in range(0, len(ds))])
+
+        diff[i] = abs(max_curr - min_curr)
+
+    diff_sum = sum(diff)
+    return diff_sum <= TARGET_DIFF_VALUE
+
+
 @app.route('/random', methods=['GET'])
 def get_random():
     ds = request.args.get('dataSet')  # Get the 'ys' parameter value
     if ds is not None:
         ds = json.loads(ds)  # Parse 'ys' as JSON
-        print(ds)
-        diff = 0
-        for d1, d2 in zip(ds[0], ds[1]):
-            diff += abs(d1-d2)
-        print(diff)
-        if diff <= 50:
+        if _evaluate(ds):
             returnVal = jsonify(1), 200
         else:
             returnVal = jsonify(0), 200
         print(returnVal)
         return returnVal
-        # return jsonify(ys), 200  # Return 'ys' as a JSON response
     else:
         return "No 'ys' parameter provided.", 400  # Bad request
 
@@ -38,5 +56,79 @@ def get_ys():
         return "No 'ys' parameter provided.", 400  # Bad request
 
 
+def get_cf_func(ds):
+    print("Does orginal work?", _evaluate(ds))
+    new_time_series = [[yVal for yVal in timeSeries] for timeSeries in ds]
+
+    diff_per_time_steps = [0]*len(new_time_series[0])
+    median = [0]*len(new_time_series[0])
+
+    for timeStep in range(len(new_time_series[0])):  # for each timeStep
+        minVal = float("inf")
+
+        maxVal = float("-inf")
+        for timeSeries in new_time_series:
+            currYVal = timeSeries[timeStep]
+
+            if currYVal < minVal:
+                minVal = currYVal
+
+            if currYVal > maxVal:
+                maxVal = currYVal
+
+        diff = abs(maxVal - minVal)
+        diff_per_time_steps[timeStep] = diff
+        median[timeStep] = (maxVal + minVal) / 2
+
+    total_diff = sum(diff_per_time_steps)
+    # How much we should move depending on the difference
+    scale = TARGET_DIFF_VALUE / total_diff
+    print("scale:", scale)
+    print("median", median)
+
+    updated_time_series = [[yVal for yVal in timeSeries]
+                           for timeSeries in new_time_series]
+    for idTStep in range(len(new_time_series[0])):  # for each timeStep
+        currMedian = median[idTStep]
+        for idTSeries, timeSeries in enumerate(new_time_series):
+            currYVal = timeSeries[idTStep]
+            distToMed = abs(currMedian-currYVal)
+            moveFromMid = distToMed*scale
+            if currYVal > currMedian:
+                updated_time_series[idTSeries][idTStep] = currMedian + moveFromMid
+            else:
+                updated_time_series[idTSeries][idTStep] = currMedian - moveFromMid
+
+    print("Worked?", _evaluate(updated_time_series))
+    return updated_time_series
+
+
+@app.route('/cf', methods=['GET'])
+def get_cf():
+    """
+    we want to find a counterfactual of the index item to make it positive
+    @return A counterfactual time series. For now we only change one time series
+    """
+    ds = request.args.get('dataSet')  # List of time series
+    # index = request.get("index")
+    if ds is None:  # or index is None:
+        return "You have to provide a 'dataSet' parameter and a 'index' parameter.", 400
+    else:
+        ds = json.loads(ds)  # Parse 'ys' as JSON
+        cf = get_cf_func(ds)
+
+        return jsonify(cf), 200
+
+
+def test():
+    ds = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+          [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]]
+    print(list([round(val, 3) for val in row] for row in ds))
+    new_ds = get_cf_func(ds)
+    print(list([round(val, 3) for val in row] for row in new_ds))
+
+
 if __name__ == '__main__':
     app.run(port=8765)  # Start the server
+    # test()
