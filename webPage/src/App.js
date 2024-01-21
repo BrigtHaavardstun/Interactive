871382877fc82d68a0ba91c81df1cd91a5a36ce4
BrigtHaavardstun <Brigt_Arve@outlook.com>
2024-01-21 17:03:01 +0100

    [app/🧹] Remove unused components

--- webPage/src/App.js
+++ webPage/src/App.js
@@ -10,10 +10,7 @@ export default () => {
       <h1>Interactive Chart, with python backend Demo</h1>
       <h2>By: Brigt</h2>
       <h4>Credit: based on https://github.com/sheykholeslam/React-Draggable-Chart-Example/tree/master</h4>
-      {/* <Swr />*/}
-      {/*<SwrRandom />*/}
       <ParentComponent />
-      {/*<CrazyLine />*/}
     </div>
   );
 };
