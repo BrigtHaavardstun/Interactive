5647927a5ce3fb4682ab98c8607134e92317c3e4
BrigtHaavardstun <Brigt_Arve@outlook.com>
2024-01-21 17:01:32 +0100

    [web/cf] Add a cf window in parant

--- webPage/src/ParentComponent.jsx
+++ webPage/src/ParentComponent.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DraggableGraph from './DraggableData';
 import axios from 'axios';
 
 
 export const ParentComponent = () => {
-    const [lineColor, setLineColor] = useState('green');
-    const [dataSet, setDataSet] = useState([
+
+    const [lineColorOrg, setLineColorOrg] = useState('green');
+    const [dataSetOrg, setDataSetOrg] = useState([
         [65, 59, 80, 81, 56, 55, 40],
         [35, 39, 50, 91, 46, 85, 10],
     ]);
-
-    console.log("Test dataset", dataSet);
-    const setColorColor = () => {  // Receive setLineColor as a prop
-        axios.get('http://localhost:8765/random', {
+    console.log("Test dataset", dataSetOrg);
+    const updateColorOrg = () => {  // Receive setLineColor as a prop
+        axios.get('http://localhost:8765/getCol', {
             params: {
-                dataSet: JSON.stringify(dataSet)  // Convert dataSet to a JSON string
+                dataSet: JSON.stringify(dataSetOrg)  // Convert dataSet to a JSON string
             }
         })
             .then((res) => {
                 // Change the color based on the response
 
                 if (res.data == 1) {
-                    setLineColor("green");
+                    setLineColorOrg("green");
                 }
                 else {
-                    setLineColor("red")
+                    setLineColorOrg("red")
                 }
 
             })
@@ -32,11 +32,63 @@ export const ParentComponent = () => {
                 console.error('Error:', error);
             });
     };
-    setColorColor();
+    useEffect(() => { updateColorOrg(); }, [dataSetOrg]);
+
+    const [dataSetCF, setDataSetCF] = useState([
+        [65, 59, 80, 81, 56, 55, 40],
+        [35, 39, 50, 91, 46, 85, 10],
+    ]);
+    const [lineColorCF, setLineColorCF] = useState('green');
+
+    const getCFData = () => {
+        axios.get('http://localhost:8765/cf', {
+            params: {
+                dataSet: JSON.stringify(dataSetOrg)  // Convert dataSet to a JSON string
+            }
+        })
+            .then((res) => {
+
+                // Display new counterfactual data
+                setDataSetCF(res.data);
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+
+    useEffect(() => { getCFData(); }, [dataSetOrg]);
+
+    const updateCFColor = () => {  // Receive setLineColor as a prop
+        axios.get('http://localhost:8765/getCol', {
+            params: {
+                dataSet: JSON.stringify(dataSetCF)  // Convert dataSet to a JSON string
+            }
+        })
+            .then((res) => {
+                // Change the color based on the response
+
+                if (res.data == 1) {
+                    setLineColorCF("green");
+                }
+                else {
+                    setLineColorCF("red")
+                }
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+    useEffect(() => { updateCFColor() }, [dataSetOrg]);
+
+
+
 
     return (
         <div>
-            <DraggableGraph dataSetOrg={dataSet} setDataSet={setDataSet} lineColor={lineColor} setRandomColor={setColorColor} />
+            <DraggableGraph dataSetOrg={dataSetOrg} setDataSet={setDataSetOrg} lineColor={lineColorOrg} setRandomColor={updateColorOrg} />
+            <DraggableGraph dataSetOrg={dataSetCF} setDataSet={setDataSetCF} lineColor={lineColorCF} setRandomColor={updateCFColor} />
         </div>
     );
 };
