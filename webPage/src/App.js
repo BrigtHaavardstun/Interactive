40631fd08e30e7c1bd7d1c5fc588983a23895251
BrigtHaavardstun <Brigt_Arve@outlook.com>
2024-02-19 14:55:52 +0100

    [cleanUp]

--- webPage/src/App.js
+++ webPage/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { ParentComponent } from "./ParentComponent";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default () => {
@@ -9,7 +9,7 @@ export default () => {
     <div className="App">
       <h1>Interactive Chart, with python backend Demo</h1>
       <h2>By: Brigt</h2>
-      <h4>Credit: based on https://github.com/sheykholeslam/React-Draggable-Chart-Example/tree/master</h4>
+      <h10>Credit: based on https://github.com/sheykholeslam/React-Draggable-Chart-Example/tree/master</h10>
       <ParentComponent />
     </div>
   );
